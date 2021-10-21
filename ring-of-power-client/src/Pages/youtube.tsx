c3400246d669a2b22d73c305da3382eabad8b699
Jorge Yakushi <jorge.yakushi@tannua.com>
2021-10-20 20:40:59 -0500

    added youtube login button

--- ring-of-power-client/src/Pages/youtube.tsx
+++ ring-of-power-client/src/Pages/youtube.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from "react";
-
+import Login from "../Common/Components/LoginYoutube";
 const Youtube: FC = () => {
   return (
     <>
-      <div className="">Youtube</div>
+      <div className="">
+        Youtube
+        <Login />
+      </div>
     </>
   );
 };
---
+++ ring-of-power-client/src/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  youtubeClientId:
+    "604376069778-cttv9k6i5nde8j70kn6ol3blspl72a5n.apps.googleusercontent.com",
+};
