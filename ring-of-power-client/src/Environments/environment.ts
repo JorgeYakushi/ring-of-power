891b7348490fe4ead38dacde1614b3536fea1626
Jorge Yakushi <jorge.yakushi@tannua.com>
2021-12-04 02:01:36 -0500

    gitignore

--- ring-of-power-client/src/Environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  youtubeClientId:
-    "401443293564-80rhc66onvqaf7g73ibrk93o6uci5c1a.apps.googleusercontent.com",
-  youtubeSecret: "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",
-  youtubeApiKey: "AIzaSyDnGCHStLetIN8L7Df7DK8-TgPc8A6iXCM",
-};
